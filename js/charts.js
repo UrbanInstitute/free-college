3153449e076f41392fa9aa8954f9aa8e71b28e58
Feng <afeng@urban.org>
2019-12-24 14:13:03 -0500

    Begin building out breakout by income under current situation. Add in column labels and legend

--- js/charts.js
+++ js/charts.js
@@ -8,9 +8,6 @@ var PCTFORMAT = d3.format(".0%");
 //     .range([0, chartDimensions.width_pg])
 //     .padding(0.4);
 
-// var yScale = d3.scaleLinear()
-//     .range([chartDimensions.height, 0]);
-
 // var colorScale = d3.scaleOrdinal()
 //     .domain(["", "no", "diff"])
 //     .range(["#1696d2", "#e3e3e3", "#fdbf11"]);
@@ -20,30 +17,39 @@ var isIE = navigator.userAgent.indexOf("MSIE") !== -1 || navigator.userAgent.ind
 
 window.createGraphic = function(graphicSelector) {
     var graphicEl = d3.select('#graphic');
-    var graphicVisEl = graphicEl.select('#vis');
+    var graphicVisEl = graphicEl.select('#chart');
     var graphicProseEl = graphicEl.select('#sections');
     var chartTitle = graphicEl.select(".chartTitle");
 
-    var margin = 20
-    var size = 600
-    var r = 6;
-    var chartSize = size - margin * 2
+    var margin = 20;
+    var width = 600,
+        height = 550;
+    var r = 5;
+    // var chartSize = size - margin * 2
     var titleHeight = d3.select(".chartTitle").node().getBoundingClientRect().height;
     var dotsData;
 
     var xScale = d3.scaleOrdinal()
-        .domain("no", "yes")
-        .range([size * 0.25, size * 0.75]);
+        .domain(["no", "yes"])
+        .range([0.25*width, 0.75*width]);
+
+    var yScale_inc = d3.scaleBand()
+        .domain(["Dep+80k", "Dep40k-80k", "Dep<=40k", "Ind+30k", "Ind15k-30k", "Ind<=15k"])
+        // .range([50, 150, 250, 350, 450, 550]);
+        .rangeRound([margin, height - margin])
+        .padding(margin);
+
 
     // actions to take on each step of our scroll-driven story
     var steps = [
         function step0() {
             d3.selectAll(".dotLabel").remove();
+            d3.select(".legend").classed("invisible", true);
 
             d3.selectAll(".student")
                 .transition()
-                .attr("cx", size / 2)
-                .attr("cy", (size - titleHeight) / 2);
+                .attr("cx", width / 2)
+                .attr("cy", (height - titleHeight) / 2);
         },
         function step1() {
             // console.log("spread out 100 dots");
@@ -55,9 +61,9 @@ window.createGraphic = function(graphicSelector) {
 
             var simulation = d3.forceSimulation(dotsData)
                 .force('charge', d3.forceManyBody().strength(-10))
-                .force('center', d3.forceCenter(size / 2, (size - titleHeight)/2))
-                .force('x', d3.forceX().x(size / 2))
-                .force('y', d3.forceY().y((size - titleHeight) / 2))
+                .force('center', d3.forceCenter(width / 2, (height - titleHeight)/2))
+                .force('x', d3.forceX().x(width / 2))
+                .force('y', d3.forceY().y((height - titleHeight) / 2))
                 .force('collision', d3.forceCollide().radius(r))
                 .stop();
                 // .on('tick', ticked);
@@ -74,6 +80,8 @@ window.createGraphic = function(graphicSelector) {
                     simulation.tick();
                 }
 
+                d3.select(".legend").classed("invisible", true);
+
                 var students = d3.selectAll(".student");
 
                 students
@@ -87,7 +95,7 @@ window.createGraphic = function(graphicSelector) {
                 var lowestDotY = d3.max(students.data(), function(d) { return d.y; });
                 d3.select("svg g").append("text")
                     .attr("class", "dotLabel")
-                    .attr("x", size / 2)
+                    .attr("x", width / 2)
                     .attr("y", lowestDotY + 60)
                     .text(dotsData.length + " students");
             });
@@ -104,7 +112,78 @@ window.createGraphic = function(graphicSelector) {
             var simulation = d3.forceSimulation(dotsData)
                 .force('charge', d3.forceManyBody().strength(-10))
                 .force('x', d3.forceX().x(function(d) { return xScale(d.currentFreeCollege); }))  // seem to need to add an adjustment factor here
-                .force('y', d3.forceY().y((size - titleHeight) / 2))
+                .force('y', d3.forceY().y((height - titleHeight) / 2))
+                .force('collision', d3.forceCollide().radius(r))
+                .stop();
+
+            d3.timeout(function() {
+              // See https://github.com/d3/d3-force/blob/master/README.md#simulation_tick
+              for (var i = 0, n = Math.ceil(Math.log(simulation.alphaMin()) / Math.log(1 - simulation.alphaDecay())); i < n; ++i) {
+                simulation.tick();
+              }
+
+                d3.select(".legend").classed("invisible", true);
+
+                var students = d3.selectAll(".student");
+
+                students
+                    .transition(t)
+                    .attr("cx", function(d) { return d.x; })
+                    .attr("cy", function(d) { return d.y; });
+
+                students.classed("noFreeCollege", function(d) { return d.currentFreeCollege === "no" ? true : false; });
+
+                // label "Free College" and "No free college" columns
+                var svg = d3.select("svg g");
+
+                svg.append("text")
+                    .attr("class", "columnLabel")
+                    .attr("x", xScale("yes"))
+                    .attr("y", margin)
+                    .text("Free college");
+
+                svg.append("text")
+                    .attr("class", "columnLabel")
+                    .attr("x", xScale("no"))
+                    .attr("y", margin)
+                    .text("No free college");
+
+                // update label below dots
+                var numFreeCollege = students.data().filter(function(d) { return d.currentFreeCollege !== "no"; }).length;
+                var numNoFreeCollege = 100 - numFreeCollege;
+
+                var lowestDotY = d3.max(students.data(), function(d) { return d.y; });
+
+                svg.append("text")
+                    .attr("class", "dotLabel")
+                    .attr("x", xScale("yes"))
+                    .attr("y", lowestDotY + 40)
+                    .text(numFreeCollege + " students");
+
+                svg.append("text")
+                    .attr("class", "dotLabel noFreeCollege")
+                    .attr("x", xScale("no"))
+                    .attr("y", lowestDotY + 40)
+                    .text(numNoFreeCollege + " students");
+            });
+        },
+        function step3() {
+        },
+        function step4() {
+            // console.log("separate into income groups");
+            d3.selectAll(".dotLabel").remove();
+
+            d3.select(".chartTitle").text("Current situation");
+            d3.select(".legend").classed("invisible", false);
+
+            var t = d3.transition()
+                .duration(800)
+                .ease(d3.easeQuadInOut)
+
+            var simulation = d3.forceSimulation(dotsData)
+                .force('charge', d3.forceManyBody().strength(-10))
+                .force('x', d3.forceX().x(function(d) { return xScale(d.currentFreeCollege); }))  // seem to need to add an adjustment factor here
+                .force('y', d3.forceY().y(function(d) { return yScale_inc(d.incomegroup); }))
                 .force('collision', d3.forceCollide().radius(r))
                 .stop();
 
@@ -124,32 +203,29 @@ window.createGraphic = function(graphicSelector) {
                 students.classed("noFreeCollege", function(d) { return d.currentFreeCollege === "no" ? true : false; });
 
                 // update label below dots
-                var numFreeCollege = students.data().filter(function(d) { return d.currentFreeCollege !== "no"; }).length;
-                var numNoFreeCollege = 100 - numFreeCollege;
+                // var numFreeCollege = students.data().filter(function(d) { return d.currentFreeCollege !== "no"; }).length;
+                // var numNoFreeCollege = 100 - numFreeCollege;
 
-                var lowestDotY = d3.max(students.data(), function(d) { return d.y; });
+                // var lowestDotY = d3.max(students.data(), function(d) { return d.y; });
 
-                d3.select("svg g")
-                    .append("text")
-                    .attr("class", "dotLabel")
-                    .attr("x", xScale("yes"))
-                    .attr("y", lowestDotY + 40)
-                    .text(numFreeCollege + " students");
+                // d3.select("svg g")
+                //     .append("text")
+                //     .attr("class", "dotLabel")
+                //     .attr("x", xScale("yes"))
+                //     .attr("y", lowestDotY + 40)
+                //     .text(numFreeCollege + " students");
 
-                d3.select("svg g")
-                    .append("text")
-                    .attr("class", "dotLabel noFreeCollege")
-                    .attr("x", xScale("no"))
-                    .attr("y", lowestDotY + 40)
-                    .text(numNoFreeCollege + " students");
+                // d3.select("svg g")
+                //     .append("text")
+                //     .attr("class", "dotLabel noFreeCollege")
+                //     .attr("x", xScale("no"))
+                //     .attr("y", lowestDotY + 40)
+                //     .text(numNoFreeCollege + " students");
             });
         },
+        function step5() {
 
-        function step3() {
-        },
-        // function step4() {
-        //     console.log()
-        // }
+        }
     ]
 
     // update our chart
@@ -182,8 +258,8 @@ window.createGraphic = function(graphicSelector) {
             dotsData = data;
 
             var svg = graphicVisEl.append('svg')
-                .attr('width', size)
-                .attr('height', size)
+                .attr('width', width)
+                .attr('height', height)
 
             var chart = svg.append('g');
                 // .attr('transform', 'translate(0,' + margin + ')')
@@ -193,8 +269,8 @@ window.createGraphic = function(graphicSelector) {
                 .enter()
                 .append('circle')
                 .attr("class", "student")
-                .attr("cx", size / 2)
-                .attr("cy", (size - titleHeight) / 2)
+                .attr("cx", width / 2)
+                .attr("cy", (height - titleHeight) / 2)
                 .attr("r", r);
         });
     }
