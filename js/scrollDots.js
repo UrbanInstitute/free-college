b43b1a15ebb2ed5032e4ca49ff1000ab932ab783
Feng <afeng@urban.org>
2020-01-14 13:27:25 -0500

    Begin implementing scroll to section when clicking on dot

---
+++ js/scrollDots.js
@@ -0,0 +1,45 @@
+// code modified from: https://github.com/CodyHouse/vertical-fixed-navigation
+$(document).ready(function($){
+    var contentSections = $('.step'),
+        navigationItems = $('#navDots a');
+
+    // updateNavigation();
+    // $(window).on('scroll', function(){
+    //     updateNavigation();
+    // });
+
+    //smooth scroll to the section when clicking on a dot
+    navigationItems.on('click', function(event){
+        event.preventDefault();
+        smoothScroll($(this.hash));
+    });
+
+    // //open-close navigation on touch devices
+    // $('.touch .cd-nav-trigger').on('click', function(){
+    //     $('.touch #cd-vertical-nav').toggleClass('open');
+
+    // });
+    // //close navigation on touch devices when selectin an elemnt from the list
+    // $('.touch #cd-vertical-nav a').on('click', function(){
+    //     $('.touch #cd-vertical-nav').removeClass('open');
+    // });
+
+    // function updateNavigation() {
+    //     contentSections.each(function(){
+    //         $this = $(this);
+    //         var activeSection = $('#cd-vertical-nav a[href="#'+$this.attr('id')+'"]').data('number') - 1;
+    //         if ( ( $this.offset().top - $(window).height()/2 < $(window).scrollTop() ) && ( $this.offset().top + $this.height() - $(window).height()/2 > $(window).scrollTop() ) ) {
+    //             navigationItems.eq(activeSection).addClass('is-selected');
+    //         }else {
+    //             navigationItems.eq(activeSection).removeClass('is-selected');
+    //         }
+    //     });
+    // }
+
+    function smoothScroll(target) {
+        $('body,html').animate(
+            {'scrollTop':target.offset().top - 100},
+            1000
+        );
+    }
+});
